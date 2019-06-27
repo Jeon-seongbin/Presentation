b7119d27967d0fcfc1f19db7d096b783fc46df70
wk0012345@naver.com <wk0012345@naver.com>
2019-06-27 23:07:15 +0900

    axios - get

--- react/code3/src/Shop.js
+++ react/code3/src/Shop.js
@@ -2,6 +2,8 @@ import React ,{useEffect, useState} from 'react';
 import './App.css';
 import {Link} from 'react-router-dom';
 
+import UserForm from './component/UserForm';
+
 function Shop() {
 
     useEffect(()=>{
@@ -26,6 +28,7 @@ function Shop() {
         {items.map( item => (
             <h1 key={item.itemid}>
                 <Link to={`shop/${item.itemid}`}>
+                {/* url Param을 사용하기 위해서 링크 뒤에 식별 가능한 값을 출력하게 한다 */}
                     {item.name}
                 </Link>
             </h1>
